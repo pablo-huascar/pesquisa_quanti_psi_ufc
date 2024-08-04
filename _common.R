d20665f27df8434ec320ed1735e0952ab2216583
pablo-huascar <pablohuascar@gmail.com>
2024-08-04 07:19:17 -0300

    ....

--- _common.R
+++ _common.R
@@ -1,13 +1,21 @@
 set.seed(1014)
+options(digits = 3)
+
+# knitr chunk options ----------------------------------------------------------
 
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  # cache = TRUE,
-  fig.retina = 2,
+  message = FALSE,
+  warning = FALSE,
+  cache = FALSE, 
+  echo = FALSE, 
+  fig.align = "center",
   fig.width = 6,
-  fig.asp = 0.618,
-  fig.show = "hold"
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  dpi = 300,
+  fig.pos = "h"
 )
 
 options(
@@ -16,12 +24,16 @@ options(
   pillar.max_footer_lines = 2,
   pillar.min_chars = 15,
   stringr.view_n = 6,
-  # Temporarily deactivate cli output for quarto
-  cli.num_colors = 0,
-  cli.hyperlink = FALSE,
   pillar.bold = TRUE,
   width = 77 # 80 - 3 for #> comment
 )
 
+
+# ggplot options ----------------------------------------------------------
+
 ggplot2::theme_set(ggplot2::theme_classic(12))
+
+
+
+
 
